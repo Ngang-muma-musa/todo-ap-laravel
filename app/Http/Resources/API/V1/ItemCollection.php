fa793a4edb2d8a4e145b0a9fd966c39e77865c12
Ngang-muma-musa <ngangmuma00@gmail.com>
2024-08-23 13:22:18 +0100

    feat: udated resource directory

--- app/Http/Resources/API/V1/ItemCollection.php
+++ app/Http/Resources/API/V1/ItemCollection.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Resources\V1;
+namespace App\Http\Resources\API\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class TodoCollection extends ResourceCollection
+class ItemCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
